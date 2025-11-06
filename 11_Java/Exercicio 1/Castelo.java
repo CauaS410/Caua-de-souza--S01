530c332273ea3134a4aef8e515f64d2b77da67c7
CauaS410 <cauadesouzaflauzino@gmail.com>
2025-11-06 18:49:46 -0300

    Castelo.java

---
+++ 11_Java/Exercicio 1/Castelo.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+public class Castelo {
+    private String nome;
+
+    private ArrayList<Divisao> divisoes;
+    public Castelo(String nome, ArrayList<Divisao> divisoes) {
+        this.nome = nome;
+        this.divisoes = divisoes;
+    }
+    public void adicionarDivisao(Divisao divisao) {
+        if (this.divisoes != null) {
+            this.divisoes.add(divisao);
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public ArrayList<Divisao> getDivisoes() {
+        return divisoes;
+    }
+
+    public void setDivisoes(ArrayList<Divisao> divisoes) {
+        this.divisoes = divisoes;
+    }
+}
