df4a628a065bd04bc6a7d1b89aa3645f48f645fe
CauaS410 <cauadesouzaflauzino@gmail.com>
2025-12-04 18:23:51 -0300

    Exercício 2.pl

--- 14_Prolog/Exercício 2.pl
+++ 14_Prolog/Exercício 2.pl
@@ -1,25 +1,86 @@
-filho(zeus, cronos).
-filho(zeus, reia).
-filho(hades, cronos).
-filho(hades, reia).
-filho(poseidon, cronos).
-filho(poseidon, reia).
+progenitor(urano, cronos).
+progenitor(gaia, cronos).
+progenitor(urano, oceano).
+progenitor(gaia, oceano).
+progenitor(cronos, zeus).
+progenitor(reia, zeus).
+progenitor(cronos, poseidon).
+progenitor(reia, poseidon).
+progenitor(cronos, hades).
+progenitor(reia, hades).
+progenitor(zeus, atena).
+progenitor(zeus, apolo).
+progenitor(hera, apolo).
+progenitor(zeus, artemis).
+progenitor(letó, artemis).
+progenitor(zeus, ares).
+progenitor(hera, ares).
+progenitor(zeus, hefesto).
+progenitor(hera, hefesto).
+progenitor(poseidon, tritao).
+progenitor(zeus, perseu).
+progenitor(dânae, perseu).
+progenitor(atena, erictônio).
 
-domina(zeus, ceu).
-domina(zeus, trovao).  
-domina(hades, submundo).
-domina(poseidon, mar).
+dominio(zeus, ceu).
+dominio(zeus, trovão).
+dominio(poseidon, mar).
+dominio(poseidon, terremoto).
+dominio(hades, submundo).
+dominio(atena, sabedoria).
+dominio(atena, guerra_estrategica).
+dominio(ares, guerra_brutal).
+dominio(apolo, sol).
+dominio(apolo, musica).
+dominio(artemis, caça).
+dominio(artemis, lua).
+dominio(hefesto, fogo).
+dominio(hefesto, forja).
+dominio(afrodite, beleza).
+dominio(afrodite, amor).
+dominio(cronos, tempo).
 
-habita(zeus, olimpo).
-habita(poseidon, mar).
-habita(hades, submundo).
+arma(zeus, raio).
+arma(poseidon, tridente).
+arma(hades, elmo_escuridao).
+arma(atena, escudo_aegis).
+arma(perseu, espada_adamantina).
 
-divindade_olimpica(X) :- 
-    filho(X, cronos), 
-    (domina(X, submundo); domina(X, ceu); domina(X, mar)).
+caracteristica(zeus, rei).
+caracteristica(hera, rainha).
+caracteristica(hades, invisivel).
+caracteristica(hefesto, ferreiro).
+caracteristica(artemis, virgem).
+caracteristica(perseu, mortal).
+
+aliado(zeus, poseidon).
+aliado(poseidon, zeus).
+aliado(atena, apolo).
+aliado(apolo, atena).
+aliado(ares, hades).
+aliado(hades, ares).
+aliado(hefesto, atena).
+aliado(atena, hefesto).
+
+local_principal(zeus, olimpo).
+local_principal(poseidon, atlântida).
+local_principal(hades, submundo).
+local_principal(apolo, olimpo).
+local_principal(artemis, florestas).
+
+divindade_olimpica(Deus) :-
+    progenitor(cronos, Deus),
+    (
+        dominio(Deus, ceu);
+        dominio(Deus, mar);
+        dominio(Deus, submundo)
+    ).
 
 deus_maior(Deus) :-
-    habita(Deus, olimpo),
-    domina(Deus, D1),
-    domina(Deus, D2),
+    local_principal(Deus, olimpo),
+    dominio(Deus, D1),
+    dominio(Deus, D2),
     D1 \= D2.
+
+
+    % deus_maior(X).
