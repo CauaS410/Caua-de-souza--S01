bbe80333e246adb1c457cd9591e2be1c32d2d11f
CauaS410 <cauadesouzaflauzino@gmail.com>
2025-12-04 17:30:15 -0300

    Exercício 2.pl

--- 15_Prolog/Exercício 2.pl
+++ 15_Prolog/Exercício 2.pl
@@ -1,29 +1,50 @@
-area(calculo_1, fundamental).
-area(algoritmos, fundamental).
-area(estatistica, fundamental).
-area(calculo_2, exatas).
-area(estrutura_dados, computacao).
-area(inteligencia_artificial, computacao).
+disciplina(programacao1, 6, fundamental).
+disciplina(calculo1, 8, matematica).
+disciplina(estrutura_dados, 6, computacao).
+disciplina(algoritmos, 4, computacao).
+disciplina(banco_dados, 6, aplicacoes).
+disciplina(redes, 4, aplicacoes).
+disciplina(inteligencia_artificial, 8, computacao).
+disciplina(compiladores, 6, computacao).
+disciplina(teoria_grafos, 4, matematica).
+disciplina(estatistica, 6, matematica).
+disciplina(sistemas_operacionais, 6, computacao).
+disciplina(projeto_integrador, 10, aplicacoes).
 
+prerequisito(estrutura_dados, programacao1).
 prerequisito(algoritmos, estrutura_dados).
-prerequisito(calculo_1, calculo_2).
-prerequisito(estatistica, inteligencia_artificial).
-prerequisito(estrutura_dados, inteligencia_artificial).
+prerequisito(banco_dados, programacao1).
+prerequisito(inteligencia_artificial, algoritmos).
+prerequisito(inteligencia_artificial, calculo1).
+prerequisito(compiladores, algoritmos).
+prerequisito(compiladores, estrutura_dados).
+prerequisito(sistemas_operacionais, estrutura_dados).
+prerequisito(teoria_grafos, calculo1).
+prerequisito(estatistica, calculo1).
+prerequisito(projeto_integrador, banco_dados).
+prerequisito(projeto_integrador, sistemas_operacionais).
 
-concluiu(joao, algoritmos).
+concluiu(maria, programacao1).
+concluiu(maria, calculo1).
+concluiu(joao, programacao1).
 concluiu(joao, estrutura_dados).
-concluiu(joao, estatistica).
-concluiu(joao, calculo_1).
-
-concluiu(maria, algoritmos).
-concluiu(maria, calculo_1).
+concluiu(joao, calculo1).
+concluiu(ana, programacao1).
+concluiu(ana, redes).
+concluiu(ana, teoria_grafos).
+concluiu(pedro, calculo1).
+concluiu(pedro, estatistica).
+concluiu(pedro, teoria_grafos).
+concluiu(lucas, estrutura_dados).
+concluiu(lucas, algoritmos).
+concluiu(lucas, redes).
+concluiu(lucas, banco_dados).
 
 falta_concluir(Aluno, Disc) :-
-    prerequisito(Disc, _),
+    prerequisito(_, Disc),
     \+ concluiu(Aluno, Disc).
 
-
 aluno_apto(Aluno, Disc) :-
-    area(Disc, Tipo),
+    disciplina(Disc, _, Tipo),
     Tipo \= fundamental,
-    \+ (prerequisito(P, Disc), falta_concluir(Aluno, P)).
+    \+ (prerequisito(Disc, P), falta_concluir(Aluno, P)).
