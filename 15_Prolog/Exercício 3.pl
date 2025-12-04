6d76ab24073b6b1ded8b5d796b67e74c1407d1cb
CauaS410 <cauadesouzaflauzino@gmail.com>
2025-12-04 17:34:34 -0300

    Exercício 3.pl

--- 15_Prolog/Exercício 3.pl
+++ 15_Prolog/Exercício 3.pl
@@ -1,24 +1,55 @@
-area(algoritmos, computacao).
-area(estrutura_dados, computacao).
-area(banco_dados, computacao).
-area(calculo_1, matematica).
-area(algebra_linear, matematica).
+disciplina(programacao1, 6, fundamental).
+disciplina(calculo1, 8, matematica).
+disciplina(estrutura_dados, 6, computacao).
+disciplina(algoritmos, 4, computacao).
+disciplina(banco_dados, 6, aplicacoes).
+disciplina(redes, 4, aplicacoes).
+disciplina(inteligencia_artificial, 8, computacao).
+disciplina(compiladores, 6, computacao).
+disciplina(teoria_grafos, 4, matematica).
+disciplina(estatistica, 6, matematica).
+disciplina(sistemas_operacionais, 6, computacao).
+disciplina(projeto_integrador, 10, aplicacoes).
 
-concluiu(lucas, algoritmos).
+prerequisito(estrutura_dados, programacao1).
+prerequisito(algoritmos, estrutura_dados).
+prerequisito(banco_dados, programacao1).
+prerequisito(inteligencia_artificial, algoritmos).
+prerequisito(inteligencia_artificial, calculo1).
+prerequisito(compiladores, algoritmos).
+prerequisito(compiladores, estrutura_dados).
+prerequisito(sistemas_operacionais, estrutura_dados).
+prerequisito(teoria_grafos, calculo1).
+prerequisito(estatistica, calculo1).
+prerequisito(projeto_integrador, banco_dados).
+prerequisito(projeto_integrador, sistemas_operacionais).
+
+concluiu(maria, programacao1).
+concluiu(maria, calculo1).
+concluiu(joao, programacao1).
+concluiu(joao, estrutura_dados).
+concluiu(joao, calculo1).
+concluiu(ana, programacao1).
+concluiu(ana, redes).
+concluiu(ana, teoria_grafos).
+concluiu(pedro, calculo1).
+concluiu(pedro, estatistica).
+concluiu(pedro, teoria_grafos).
 concluiu(lucas, estrutura_dados).
-
-concluiu(maria, algoritmos).
-concluiu(maria, calculo_1).
-
-concluiu(joao, algoritmos).
+concluiu(lucas, algoritmos).
+concluiu(lucas, redes).
+concluiu(lucas, banco_dados).
 
 especialista_comp(Aluno) :-
     concluiu(Aluno, D1),
-    area(D1, computacao),
+    disciplina(D1, _, computacao),
     concluiu(Aluno, D2),
-    area(D2, computacao),
+    disciplina(D2, _, computacao),
     D1 \= D2.
 
 deficiencia_mat(Aluno) :-
-    concluiu(Aluno, _),
-    \+ (concluiu(Aluno, D), area(D, matematica)).
+    \+ (concluiu(Aluno, D), disciplina(D, _, matematica)).
+
+consulta_exercicio_3(Aluno) :-
+    especialista_comp(Aluno),
+    deficiencia_mat(Aluno).
